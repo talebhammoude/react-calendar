2b7bf22205ebf5737dae205c35af6bf071fce081
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-02-13 20:58:42 +0100

    Update Exampleform.jsx

--- reactcalendarcomp/src/components/Exampleform/Exampleform.jsx
+++ reactcalendarcomp/src/components/Exampleform/Exampleform.jsx
@@ -31,7 +31,7 @@ function Exampleform(props) {
 
       <div className='backdrop' />
       <div className='form-dialog' >
-      <Form   name="contact" method="post"   >
+      <Form   name="contact" method="post"  onSubmit={props.addBooking} >
       <input type="hidden" name="form-name" value="contact"  />
 
       <Row className="mb-4">
@@ -121,7 +121,7 @@ function Exampleform(props) {
       </Row>
 
 
-      <Button className='btn-primary' type="submit"  onClick={props.addBooking}>Skicka iväg förfrågan</Button>
+      <Button className='btn-primary' type="submit"  >Skicka iväg förfrågan</Button>
       <Button className='btn-primary' onClick={props.cancelForm}>Avbryt</Button>
 
     
