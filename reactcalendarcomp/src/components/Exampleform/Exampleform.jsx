8991c0c02018beaacdc2de979405924cc9d5f52d
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-02-19 13:03:12 +0100

    c

--- reactcalendarcomp/src/components/Exampleform/Exampleform.jsx
+++ reactcalendarcomp/src/components/Exampleform/Exampleform.jsx
@@ -1,39 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useState } from 'react';
 import {Form , Button, Row, Col} from "react-bootstrap";
 import "./Exampleform.css";
 // import React, { useState } from 'react';
 
 
-const encode = (data) => {
-  return Object.keys(data)
-      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-      .join("&");
-}
 
 
 function Exampleform(props) {
   
   
-const [body , setBody]= useState({ name: "", email: "", message: "" });
+
   
-
-
-const handleSubmit = e => {
-  fetch("/", {
-    method: "POST",
-    headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    body: encode({ "name": "contact", ...body })
-  })
-    .then(() => {
-      console.log("Success!");
-    })
-    .catch(error => console.log(error));
-
-  e.preventDefault();
-};
-
-const handleChange = e => setBody({ [e.target.name]: e.target.value });
 
 
   return (
@@ -44,8 +21,8 @@ const handleChange = e => setBody({ [e.target.name]: e.target.value });
 
 
       
-      <Form   onSubmit={handleSubmit}  >
-      {/* <input type="hidden" name="form-name" value="contact"  /> */}
+      <Form  name="contact" method="post" onSubmit={props.addBooking}  >
+      <input type="hidden" name="form-name" value="contact"  />
 
       <Row className="mb-4">
         <h2>Skicka en förfrågan</h2>
@@ -62,7 +39,6 @@ const handleChange = e => setBody({ [e.target.name]: e.target.value });
             placeholder="..."
             defaultValue=""
             name="Förnamn:"
-            onChange={handleChange}
           />
           <Form.Control.Feedback></Form.Control.Feedback>
         </Form.Group>
@@ -74,14 +50,14 @@ const handleChange = e => setBody({ [e.target.name]: e.target.value });
             placeholder="..."
             defaultValue=""
             name="Efternamn:"
-            onChange={handleChange}
+            
           />
           <Form.Control.Feedback></Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group as={Col} md="6" >
           <Form.Label>E-post</Form.Label>
-          <Form.Control type="email" placeholder="namn@exempel.com" required name="E-post:" onChange={handleChange}/>
+          <Form.Control type="email" placeholder="namn@exempel.com" required name="E-post:"/>
           <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
         </Form.Group>
        
@@ -93,11 +69,11 @@ const handleChange = e => setBody({ [e.target.name]: e.target.value });
       <Row className="mb-3">
         <Form.Group as={Col} md="6" >
           <Form.Label >Önskad datum</Form.Label>
-          <Form.Control  type="text"   required value={props.dayValue} readOnly  name="Datum:" onChange={handleChange} />
+          <Form.Control  type="text"   required value={props.dayValue} readOnly  name="Datum:"  />
         </Form.Group>
         <Form.Group as={Col} md="6" controlId="validationCustom04">
           <Form.Label>Välj önskad tid</Form.Label>
-          <Form.Select name="Tid:"  onChange={handleChange}>
+          <Form.Select name="Tid:" >
             <option id="no-options" value="no-value"></option>
             <option id="12:00 - 13:00" value="12:00 - 13:00">12:00 - 13:00</option>
             <option id="13:10 - 14:10" value="13:10 - 14:10">13:10 - 14:10</option>
@@ -115,7 +91,7 @@ const handleChange = e => setBody({ [e.target.name]: e.target.value });
       <Row className="mb-2">
         <Form.Group className="mb-1"  >
         <Form.Label>Beskriv ditt fall</Form.Label>
-        <Form.Control id="description1" as="textarea" rows={2} name="Beskrivning:" onChange={handleChange} required/>
+        <Form.Control id="description1" as="textarea" rows={2} name="Beskrivning:"  required/>
         </Form.Group>
       </Row>
 
