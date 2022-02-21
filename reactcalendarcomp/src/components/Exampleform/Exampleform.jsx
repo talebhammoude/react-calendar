757fad33ea4b0fb8e8d580ab7ba8d3221ecdc3ea
Taleb Hammoudé <taleb.hammoude@outlook.com>
2022-02-21 14:13:52 +0100

    another way to post form

--- reactcalendarcomp/src/components/Exampleform/Exampleform.jsx
+++ reactcalendarcomp/src/components/Exampleform/Exampleform.jsx
@@ -1,15 +1,66 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Form , Button, Row, Col} from "react-bootstrap";
 import "./Exampleform.css";
-// import React, { useState } from 'react';
+import {useState, useEffect} from 'react'
 
 
 
 
 function Exampleform(props) {
   
+  // Hjälp från https://dev.to/gamil91/function-component-react-form-submission-on-netlify-gab
   
+  const [formData, setFormData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    date: props.dayValue,
+    time: "",
+    descr: ""
+});
 
+
+const [isSubmitted, setIsSubmitted] = useState(false);
+
+
+const handleSubmit = e => {
+  setIsSubmitted(true)
+  e.preventDefault();
+  }
+
+
+const handleChange = e => {
+  const { name, value } = e.target
+  setFormData({...formData, 
+            [name]: value
+        })
+    }
+
+
+    const encode = (data) => {
+      return Object.keys(data)
+          .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+          .join("&");
+    }
+
+
+
+    useEffect(() => {
+      if(Object.keys(isSubmitted)){
+
+          fetch("/", {
+              method: "POST",
+              headers: { "Content-Type": "application/x-www-form-urlencoded" },
+              body: encode({ "form-name": "contact-form", ...formData })
+          })
+          .then(() => alert("Success!"))
+          .then(() => setIsSubmitted(false))
+         // .then(() => setFormData({name: "", email: "",  message: ""}))
+          .catch(error => alert(error))
+      }
+  }, [formData, isSubmitted])
+
+    
   
 
 
@@ -21,8 +72,8 @@ function Exampleform(props) {
 
 
       
-      <Form  name="contact" method="post" onSubmit={props.addBooking}  >
-      <input type="hidden" name="form-name" value="contact"  />
+      <Form  onSubmit={handleSubmit}  >
+      {/* <input type="hidden" name="form-name" value="contact"  /> */}
 
       <Row className="mb-4">
         <h2>Skicka en förfrågan</h2>
@@ -39,6 +90,8 @@ function Exampleform(props) {
             placeholder="..."
             defaultValue=""
             name="Förnamn:"
+            value={formData.firstname} 
+            onChange={handleChange}
           />
           <Form.Control.Feedback></Form.Control.Feedback>
         </Form.Group>
@@ -50,14 +103,15 @@ function Exampleform(props) {
             placeholder="..."
             defaultValue=""
             name="Efternamn:"
-            
+            value={formData.lastname} 
+            onChange={handleChange}
           />
           <Form.Control.Feedback></Form.Control.Feedback>
         </Form.Group>
 
         <Form.Group as={Col} md="6" >
           <Form.Label>E-post</Form.Label>
-          <Form.Control type="email" placeholder="namn@exempel.com" required name="E-post:"/>
+          <Form.Control type="email" placeholder="namn@exempel.com" required name="E-post:" value={formData.email}  onChange={handleChange}/>
           <Form.Control.Feedback type="invalid"></Form.Control.Feedback>
         </Form.Group>
        
@@ -69,11 +123,11 @@ function Exampleform(props) {
       <Row className="mb-3">
         <Form.Group as={Col} md="6" >
           <Form.Label >Önskad datum</Form.Label>
-          <Form.Control  type="text"   required value={props.dayValue} readOnly  name="Datum:"  />
+          <Form.Control  type="text"   required value={formData.date}  readOnly  name="Datum:" onChange={handleChange} />
         </Form.Group>
         <Form.Group as={Col} md="6" controlId="validationCustom04">
           <Form.Label>Välj önskad tid</Form.Label>
-          <Form.Select name="Tid:" >
+          <Form.Select name="Tid:" value={formData.time}  onChange={handleChange}>
             <option id="no-options" value="no-value"></option>
             <option id="12:00 - 13:00" value="12:00 - 13:00">12:00 - 13:00</option>
             <option id="13:10 - 14:10" value="13:10 - 14:10">13:10 - 14:10</option>
@@ -91,7 +145,7 @@ function Exampleform(props) {
       <Row className="mb-2">
         <Form.Group className="mb-1"  >
         <Form.Label>Beskriv ditt fall</Form.Label>
-        <Form.Control id="description1" as="textarea" rows={2} name="Beskrivning:"  required/>
+        <Form.Control id="description1" as="textarea" rows={2} name="Beskrivning:" value={formData.descr}   required onChange={handleChange}/>
         </Form.Group>
       </Row>
 
